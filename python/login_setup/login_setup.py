2b8fc98b60cff25d874f44c672e81c9a60074144
Lozik88 <jarrellporter@gmail.com>
2021-12-28 14:50:56 -0600

    added functions to encode/decode str, int and float to bytes. Started working on TOML config functions

--- python/login_setup/login_setup.py
+++ python/login_setup/login_setup.py
@@ -1,13 +1,56 @@
 import json
 import os
 import re
+from sqlite3.dbapi2 import Binary
 import pandas as pd
 import sqlite3
 import numpy as np
+import toml 
+
+from struct import pack, unpack
 from cryptography.fernet import Fernet
 
-# os.path.dirname(__file__)
-def _dump_keys(d,desc,path):
+def encode(obj):
+    """
+    Takes string or integer. Returns bytes value.
+    """
+    if isinstance(obj,(int,float)):
+        obj = str(obj)
+    obj=bytes(obj,'ascii')
+    return obj 
+def decode(byte:bytes):
+    """
+    Takes bytes value. Returns original value.
+    """
+    v = byte.decode('ascii')
+    if v.replace(".","",1).isdigit():
+        if len([i for i in v if i=="."])==1:
+            v=float(v)
+        else:
+            v=int(v)
+    return v
+
+def encrypt(obj):
+    """
+    Returns dictionary of public and private key using cryptography.fernet.
+    Encrypt a string/integer OR a dict. If dict, each value will be encrypted 
+    and stored in the returned dictionary.
+    """
+    bytes()
+    keys={}
+    key = Fernet.generate_key()
+    f = Fernet(key)
+    keys["key_prv"] = key.decode('utf-8')
+    if isinstance(obj,(str,int)):
+        keys["key_pub"] = f.encrypt(encode(obj)).decode('utf-8')
+    elif isinstance(obj,dict):
+        for k,v in obj.items():
+            keys[k]=f.encrypt(bytes(v)).decode('utf-8')
+    return keys
+
+def decrypt(key:str,value):
+    pass
+def _dump_key_file(d,desc,path):
     if not os.path.exists(path):
         with open(path,'w') as f:
             json.dump([d],f,indent=2)
@@ -29,7 +72,7 @@ def _dump_keys(d,desc,path):
             else:
                 j.append(d)
             json.dump(j,f,indent=2)
-def make_keys(
+def make_key_file(
     desc:str
     ,username:str
     ,pw:str
@@ -57,6 +100,8 @@ def make_keys(
     f = Fernet(key)
     username = f.encrypt(username.encode('ascii'))
     pw = f.encrypt(pw.encode('ascii'))
+    username = encrypt(username)
+    pw = encrypt(pw)
     pub={
         'desc':desc,
         'username':username.decode('utf-8'),
@@ -66,11 +111,96 @@ def make_keys(
         'desc':desc,
         'key':key.decode('utf-8')
     }
-    _dump_keys(prv,desc,prv_path)
-    _dump_keys(pub,desc,pub_path)
+    _dump_key_file(prv,desc,prv_path)
+    _dump_key_file(pub,desc,pub_path)
 
-path = os.path.dirname(__file__)
-prv_path = os.path.join(path,'keys_prv.json')
-pub_path = os.path.join(path,'keys_pub.json')
+def read_key_file(
+    conf_path:str=None
+):
+    pass
+def make_config(conf_path:str,d:dict):
+    """
+    Creates a new TOML configuration file. This will overwrite if there's an existing file.
+    """
+    with open(conf_path,'w') as f:
+        toml_str = toml.dump(d,f)
 
-make_keys('test keys3','lozik','12345test!%4$',path,path)
+def read_config(conf_path:str):
+    """
+    Reads and existing TOML file.
+    """
+    with open(conf_path,'r') as f:
+        return toml.load(f) 
+
+def add_server_config(
+    conf_path
+    ,env:str
+    ,server_name:str
+    ,dbname:str
+    ,port:int
+    ,username:str=None
+    ,pw:str=None
+    ):
+    """
+    Adds a server to the TOML configuration file.
+    """
+
+conf={
+    'paths':{
+        "public_keys":'public keyfile path'
+    },
+    'servers':{
+        "SqlServer1":{
+            "prod":{
+                "ServerAddress":"192.168.0.1",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue"
+            },
+            "stage":{
+                "ServerAddress":"192.168.0.1",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue" 
+            },
+            "dev":{
+                "ServerAddress":"192.168.0.1",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue" 
+            }
+        },
+        "SqlServer2":{
+            "prod":{
+                "ServerAddress":"localhost",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue",
+            },
+            "stage":{
+                "ServerAddress":"localhost",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue",
+            },
+            "dev":{
+                "ServerAddress":"localhost",
+                "Database":"Adventureworks2021",
+                "Username":"someUserName",
+                "password":"someHashedValue",
+            }
+        }
+    }
+}
+# path = os.path.dirname(__file__)
+# conf_path = '/home/lozik/Desktop/conf.toml'
+# make_config(conf_path,conf)
+# # prv_path = os.path.join(path,'keys_prv.json')
+# # pub_path = os.path.join(path,'keys_pub.json')
+# # make_keys('test keys3','lozik','12345test!%4$',path,path)
+# {"username":'lozik',"pw":'12345test!%4$',"pin":12345}
+# toml_dict = read_config(conf_path)
+# esingle = encrypt("some value")
+# edict = encrypt({"test1":""})
+
+""
