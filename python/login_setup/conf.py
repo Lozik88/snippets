4b6cf1a27d3531b04bd3b139801d8bc0ddb45932
Lozik88 <jarrellporter@gmail.com>
2021-12-29 16:11:48 -0600

    created function config library in conf.py. abandoning login_setup.py

--- python/login_setup/conf.py
+++ python/login_setup/conf.py
@@ -7,6 +7,7 @@ import sqlite3
 import numpy as np
 import toml 
 import getpass
+from pandas import DataFrame
 
 from cryptography.fernet import Fernet
 
@@ -45,87 +46,6 @@ def decrypt(key:bytes,value):
     if not isinstance(value,bytes):
         value=encode(value)
     return f.decrypt(value)
-
-# def _dump_key_file(d,desc,path):
-#     if not os.path.exists(path):
-#         with open(path,'w') as f:
-#             json.dump([d],f,indent=2)
-#     else:
-#         with open(path) as f:
-#             j = json.load(f)
-#             rm_idx=None
-#             for idx,i in enumerate(j):
-#                 for k,v in i.items():
-#                     if k=='desc' and v==desc:
-#                         rm_idx = idx
-#                         break
-#                 if rm_idx !=None:
-#                     break
-#         with open(path,'w') as f:
-#             if rm_idx !=None:
-#                 j.pop(rm_idx)
-#                 j.append(d)
-#             else:
-#                 j.append(d)
-#             json.dump(j,f,indent=2)
-# def encrypt(obj):
-#     """
-#     Returns dictionary of public and private key using cryptography.fernet.
-#     Encrypt a string/integer OR a dict. If dict, each value will be encrypted 
-#     and stored in the returned dictionary.
-#     """
-#     keys={}
-#     key = Fernet.generate_key()
-#     f = Fernet(key)
-#     keys["key_prv"] = key.decode('utf-8')
-#     if isinstance(obj,(str,int)):
-#         keys["key_pub"] = f.encrypt(encode(obj)).decode('utf-8')
-#     elif isinstance(obj,dict):
-#         for k,v in obj.items():
-#             keys[k]=f.encrypt(bytes(v)).decode('utf-8')
-#     return keys
-# def make_key_file(
-#     desc:str
-#     ,username:str
-#     ,pw:str
-#     ,prv_path:str
-#     ,pub_path:str
-#     ,prv_fname:str=None
-#     ,pub_fname:str=None
-#     ):
-#     if os.path.isfile(prv_path):
-#         raise ValueError(f"{prv_path} must be a directory.")
-#     if os.path.isfile(pub_path):
-#         raise ValueError(f"{pub_path} must be a directory.")
-#     if not os.path.isdir(prv_path):
-#         raise ValueError(f"Directory {prv_path} does not exist.")
-#     if not os.path.isdir(pub_path):
-#         raise ValueError(f"Directory {pub_path} does not exist.")
-#     if prv_fname == None:
-#         prv_fname = 'keys.json'
-#     if pub_fname == None:
-#         pub_fname = 'keys_pub.json'
-#     prv_path=os.path.join(prv_path,prv_fname)
-#     pub_path=os.path.join(pub_path,pub_fname)
-
-#     key = Fernet.generate_key()
-#     f = Fernet(key)
-#     username = f.encrypt(username.encode('ascii'))
-#     pw = f.encrypt(pw.encode('ascii'))
-#     username = encrypt(username)
-#     pw = encrypt(pw)
-#     pub={
-#         'desc':desc,
-#         'username':username.decode('utf-8'),
-#         'password':pw.decode('utf-8')
-#     }
-#     prv={
-#         'desc':desc,
-#         'key':key.decode('utf-8')
-#     }
-#     _dump_key_file(prv,desc,prv_path)
-#     _dump_key_file(pub,desc,pub_path)
-
 def _check_overwrite(path,overwrite):
     "Overwrite protection. Throws an error if the file exists and overwrite is set to False."
     if os.path.exists(path) and overwrite==False:
@@ -261,72 +181,39 @@ def get_server_info(
 make_config_shell(overwrite=True) #should pass
 for v in ['prod','stage','dev']:
     make_key_file(v,f'{v}_key.json','config.toml',True)
-config_server('prod',"SqlServer1","localhost",'AdventureWorks2021')
-print(get_server_info('prod','SqlServer1'))
-config_server('prod',"SqlServer1","localhost",'AdventureWorks2021')
-print(get_server_info('prod','SqlServer1'))
 
-config_server('prod',"SqlServer2","loc192.alhost",'okok')
-print(get_server_info('prod','SqlServer2'))
-config_server('prod',"SqlServer2","c192.al",'okok')
-print(get_server_info('prod','SqlServer1'))
+lst=[]
 
-conf={
-    'paths':{
-        "public_keys":'public keyfile path'
-    },
-    'servers':{
-        "SqlServer1":{
-            "prod":{
-                "ServerAddress":"192.168.0.1",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue"
-            },
-            "stage":{
-                "ServerAddress":"192.168.0.1",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue" 
-            },
-            "dev":{
-                "ServerAddress":"192.168.0.1",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue" 
-            }
-        },
-        "SqlServer2":{
-            "prod":{
-                "ServerAddress":"localhost",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue",
-            },
-            "stage":{
-                "ServerAddress":"localhost",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue",
-            },
-            "dev":{
-                "ServerAddress":"localhost",
-                "Database":"Adventureworks2021",
-                "Username":"someUserName",
-                "password":"someHashedValue",
-            }
-        }
-    }
-}
-# path = os.path.dirname(__file__)
-# conf_path = '/home/lozik/Desktop/config.toml'
-# make_config(conf_path,conf)
-# # prv_path = os.path.join(path,'keys_prv.json')
-# # pub_path = os.path.join(path,'keys_pub.json')
-# # make_keys('test keys3','lozik','12345test!%4$',path,path)
-# {"username":'lozik',"pw":'12345test!%4$',"pin":12345}
-# toml_dict = read_config(conf_path)
-# esingle = encrypt("some value")
-# edict = encrypt({"test1":""})
+config_server('prod',"SqlServer1","localhost",'AdventureWorks2021',username='lozik',pw='afca1')
+lst.append(get_server_info('prod','SqlServer1'))
+config_server('prod',"SqlServer1","localhost",'AdventureWorks2021',username='lozik',pw='afc#$a1')
+lst.append(get_server_info('prod','SqlServer1'))
 
+config_server('dev',"SqlServer1","localhost",'AdventureWorks2021',username='lozidfk',pw='af2ec#$a1')
+lst.append(get_server_info('dev','SqlServer1'))
+config_server('dev',"SqlServer1","localhost",'AdventureWorks2021',username='loziffdfk',pw='af32ec#$a1')
+lst.append(get_server_info('dev','SqlServer1'))
+
+config_server('stage',"SqlServer1","localhost",'AdventureWorks2021',username='lozidfk',pw='afoook2e%#$a1')
+lst.append(get_server_info('stage','SqlServer1'))
+config_server('stage',"SqlServer1","localhost",'AdventureWorks2021',username='loziffdfk',pw='affypck#$a1')
+lst.append(get_server_info('stage','SqlServer1'))
+
+
+config_server('prod',"SqlServer2","loc192.alhost",'okok',username='lozidfk',pw=r'afzzzzzz$#@%%*c')
+lst.append(get_server_info('prod','SqlServer2'))
+config_server('prod',"SqlServer2","c192.al",'okok',username='lozidfk',pw=r'QQQ2ec#$($*%a1')
+lst.append(get_server_info('prod','SqlServer2'))
+
+config_server('dev',"SqlServer2","localhost",'AdventureWorks2021',username='lozidfk',pw='af2ec#$a1')
+lst.append(get_server_info('dev','SqlServer2'))
+config_server('dev',"SqlServer2","localhost",'AdventureWorks2021',username='loziffdfk',pw='af32ec#$a1')
+lst.append(get_server_info('dev','SqlServer2'))
+
+config_server('stage',"SqlServer2","localhost",'AdventureWorks2021',username='lozidfk',pw='afoook2e%#$a1')
+lst.append(get_server_info('stage','SqlServer2'))
+config_server('stage',"SqlServer2","localhost",'AdventureWorks2021',username='loziffdfk',pw='affypck#$a1')
+lst.append(get_server_info('stage','SqlServer2'))
+
+df = DataFrame(lst)
 ""
