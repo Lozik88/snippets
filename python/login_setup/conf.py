4e7434ace5d1e80791c0f22326bb6dd2d47331f3
Lozik88 <jarrellporter@gmail.com>
2022-01-02 17:43:26 -0600

    made apis functional. condensed some code. more refactor needed

--- python/login_setup/conf.py
+++ python/login_setup/conf.py
@@ -24,8 +24,9 @@ def encode(obj):
     if isinstance(obj,(int,float,str)):
         obj = bytes(str(obj),'utf-8')
     else:
-        raise ValueError("Value must be string, float, or int.") 
+        raise Exception(f"Value must be string, float, or int. Not {type(obj)}.") 
     return obj
+
 def decode(byte:bytes):
     """
     Takes bytes value. Returns original value.
@@ -37,54 +38,74 @@ def decode(byte:bytes):
         else:
             v=int(v)
     return v
+
 def encrypt(key:bytes,value:object):
+    """
+    Encrypts a value using a Fernet key.
+    """
     f = Fernet(key)
     value = encode(value)
-    return f.encrypt(value)   
-def decrypt(key:bytes,value):
+    return decode(f.encrypt(value))
+
+def decrypt(key:bytes,value:object):
+    """
+    Encrypts a value using a Fernet key.
+    """
     f = Fernet(key)
     if not isinstance(value,bytes):
         value=encode(value)
-    return f.decrypt(value)
+    return decode(f.decrypt(value))
+
 def _check_overwrite(path,overwrite):
     "Overwrite protection. Throws an error if the file exists and overwrite is set to False."
     if os.path.exists(path) and overwrite==False:
         raise IOError(
             f"""{path} already exists. Raising exception as a precaution. 
             Set overwrite to True to overwrite the file. Ensure the old file is backed up.""")
-def _check_element(conf:dict,value:str,check_type:str):
-    """
-    Returns empty dictionary if no case insensitve dups are present in conf keys. 
-    """
-    matches = len([k for k in conf.keys() if value.lower() == k.lower()])
-    if check_type.lower() == 'dups':
-        if matches==1:
-            raise ValueError(f"{value} already exists. Use a different name or update the contents of the element.")
-        else:
-            return {}
-    elif check_type.lower() == 'exists':
-        if matches != 1:
-            raise ValueError(f'{value} is not an existing key. Check the TOML file.')
-    else:
-        raise ValueError(f"{check_type} is an invalid input.")
+def _check_element(conf:dict,env:str,group:str,lookup_value:str):
+    d = {}
+    matches = [k for k in conf[group].keys() if lookup_value.lower() == k.lower()]
+    if len(matches)==0:
+        # return None
+        raise Exception(f"{lookup_value} doesn't exist. Current profile options under {group}: {', '.join(conf[group])}")
+    elif len(matches)==1:
+        lookup_value = matches[0]
+        if not env.lower() in [k.lower() for k in conf[group][lookup_value].keys()]:
+            raise Exception(f"{env} environment does not exist in {lookup_value} profile. Current options under {group}.{lookup_value}: {', '.join(conf[group][lookup_value])}")
+        return lookup_value
+    elif len(matches)>1:
+        raise Exception(f"Duplicates exist: {matches}. Consider revising the TOML")
+    return d
+
+def _make_element(conf:dict,env:str,group:str,profile:str):
+    if not group in conf.keys():
+        conf[group] = {}
+    if not profile.lower() in [v.lower() for v in conf[group].keys()]:
+        conf[group][profile] = {}
+    conf[group][profile][env] = {}
+    return conf
+
 def _check_env(env:str):
     """
     Validate incoming environment. Throw an error if incoming param is not in list.
     """
     if (['prod','stage','dev']).count(env.lower()) == 0:
-        raise ValueError('env parameter must be prod, stage, or dev')
+        raise Exception('env parameter must be prod, stage, or dev')
+        
 def _write_config(conf_path:str,d:dict):
     """
     Write to TOML file. Returns TOML formatted string.
     """
     with open(conf_path,'w') as f:
         return toml.dump(d,f)
+
 def read_config(conf_path:str):
     """
     Reads and existing TOML file. Returns dictionary
     """
     with open(conf_path,'r') as f:  
         return toml.load(f) 
+
 def read_key_file(
     env:str,
     conf_path:str='config.toml'
@@ -95,10 +116,11 @@ def read_key_file(
     _check_env(env)
     conf = read_config(conf_path)
     if 'private_keys' not in conf['paths'].keys():
-        raise ValueError(f"path.private_keys does not exist in {conf_path} consider using make_key_file(), or manually add it.")
+        raise Exception(f"path.private_keys does not exist in {conf_path} consider using make_key_file(), or manually add it.")
     key_path = conf['paths']['private_keys'][env]['path']
     with open(key_path,'r') as f:
         return encode(json.load(f)['key'])
+
 def make_key_file(
     env:str
     ,key_path:str
@@ -119,6 +141,7 @@ def make_key_file(
          conf['paths']['private_keys'][env]={}
     conf['paths']['private_keys'][env]['path'] = key_path    
     _write_config(conf_path,conf)
+
 def make_config_shell(conf_path:str='config.toml',overwrite:bool=False):
     """
     Creates a new TOML configuration file using a predefined template.
@@ -138,6 +161,7 @@ def make_config_shell(conf_path:str='config.toml',overwrite:bool=False):
     },'servers':{},'apis':{}}
     _write_config(conf_path,template)
     return _write_config(conf_path,template)
+
 def config_server(
     env:str
     ,server_name:str
@@ -155,24 +179,21 @@ def config_server(
     _check_env(env)
     with open(conf_path) as f:
         conf = toml.load(f)
-    if not 'servers' in conf.keys():
-        conf['servers'] = {}
-    conf['servers'][server_name] = _check_element(conf['servers'],'server_name','dups')
+    conf = _make_element(conf,env,'servers',server_name)
     if username == None:
         username = getpass.getpass('Enter username.')
     if pw == None:
         pw = getpass.getpass('Enter password.')
     key = read_key_file(env,conf_path)
-    # if not server_name.lower() in [k.lower() for k in conf['servers'].keys()]:
-    #     conf['servers'][server_name]={}
     conf['servers'][server_name][env]={
             "address":server_address,
             "database":dbname,
             "port":port,
-            "username":decode(encrypt(key,username)),
-            "password":decode(encrypt(key,pw))
+            "username":encrypt(key,username),
+            "password":encrypt(key,pw)
         }
     _write_config(conf_path,conf)
+
 def update_server_creds(
     env:str
     ,server_name:str
@@ -191,16 +212,17 @@ def update_server_creds(
     """
     _check_env(env)
     conf = read_config(conf_path)
-    _check_element(conf['servers'],server_name,'exists')
-    server_name = [k for k in conf['servers'].keys() if server_name.lower() == k.lower()][0]
+    # replace server_name variable and replace using existing keyname in TOML to match case.
+    server_name = _check_element(conf,env,'servers',server_name)
     if username == None:
         username = getpass.getpass('Enter username.')
     if pw == None:
         pw = getpass.getpass('Enter password.')
     key = read_key_file(env,conf_path)
-    conf['servers'][server_name][env]['username'] = decode(encrypt(key,username))
-    conf['servers'][server_name][env]['password'] = decode(encrypt(key,pw))
-    _write_config(conf_path,conf)   
+    conf['servers'][server_name][env]['username'] = encrypt(key,username)
+    conf['servers'][server_name][env]['password'] = encrypt(key,pw)
+    _write_config(conf_path,conf)
+
 def get_server_info(
     env:str
     ,server_name:str
@@ -211,16 +233,18 @@ def get_server_info(
     """
     _check_env(env)
     conf = read_config(conf_path)
+    server_name = _check_element(conf,env,'servers',server_name)
     if not server_name.lower() in [k.lower() for k in conf['servers'].keys()]:
-        raise ValueError(f"{server_name} was not found in the TOML file. Check the spelling, or add the server using add_server_config()")
+        raise Exception(f"{server_name} was not found in the TOML file. Check the spelling, or add the server using add_server_config()")
     if not env.lower() in [k.lower() for k in conf['servers'][server_name].keys()]:
-        raise ValueError(f"{server_name}'s {env} environment was not found in the TOML file. Use add_server_config() to add the server info for {env}")
+        raise Exception(f"{server_name}'s {env} environment was not found in the TOML file. Use add_server_config() to add the server info for {env}")
     data = conf['servers'][server_name][env]
     key = read_key_file(env,conf_path)
     data['server_name'] = server_name
-    data['username'] = decode(decrypt(key,encode(data['username'])))
-    data['password'] = decode(decrypt(key,encode(data['password'])))
+    data['username'] = decrypt(key,data['username'])
+    data['password'] = decrypt(key,data['password'])
     return data
+
 def config_api(
     env:str,
     api_name:str,
@@ -242,29 +266,57 @@ def config_api(
     encrypted_auth={}
     if auth_creds != None:
         for k,v in auth_creds.items():
-            encrypted_auth[k] = decode(encrypt(key,v))
+            encrypted_auth[k] = encrypt(key,v)
     if not api_name.lower() in [k.lower() for k in conf['apis'].keys()]:
         conf['apis'][api_name]={}
     conf['apis'][api_name][env]={
             "url":url,
-            # "endpoint":endpoint,
             "auth_creds":encrypted_auth,
             "auth_url":auth_url,
             "token_url":token_url
         }
     _write_config(conf_path,conf)
+
 def update_api_auth(
     env:str,
-    auth:dict,
-    conf_path:str='config.path'
+    api:str,
+    auth_creds:dict,
+    conf_path:str='config.toml'
+    ):
+    _check_env(env)
+    conf = read_config(conf_path)
+    api = _check_element(conf,env,'apis',api)
+    key = read_key_file(env,conf_path)
+    encrypted_auth_creds={}
+    for k,v in auth_creds.items():
+        encrypted_auth_creds[k]=encrypt(key,v)
+    conf['apis'][api][env]['auth_creds'] = encrypted_auth_creds
+    _write_config(conf_path,conf)
 
-):
+def get_api_info(    
+    env:str
+    ,api:str
+    ,conf_path:str='config.toml'
+    ):
+    """
+    Returns a dictionary of containing server information.
+    """
+    _check_env(env)
+    conf = read_config(conf_path)
+    api = _check_element(conf,env,'apis',api)
+    data = conf['apis'][api][env]
+    key = read_key_file(env,conf_path)
+    data['api'] = api
+    auth_creds={}
+    for k,v in data['auth_creds'].items():
+        auth_creds[k]=decrypt(key,v)
+    data['auth_creds'] = auth_creds
+    return data
+def get_info():
     pass
-
-
 make_config_shell(overwrite=True) #should pass
 for v in ['prod','stage','dev']:
-    make_key_file(v,f'{v}_key.json','config.toml',True)
+    make_key_file(v,f'/home/lozik/.auth/{v}_key.json','config.toml',True)
 
 lst=[]
 
@@ -287,7 +339,7 @@ lst.append(get_server_info('prod','SqlServer1'))
 
 df = DataFrame(lst)
 config_api(
-    'prod'
+    'prod'  
     ,'Kroger'
     ,url='https://api.kroger.com/v1/'
     ,auth_creds={'client_id':'lozik','client_secret':'someOauth2pw1'}
@@ -295,4 +347,8 @@ config_api(
     ,token_url='https://api.kroger.com/v1/connect/oauth2/token'
 )
 
+print(get_api_info('prod','kroger'))
+update_api_auth('prod','kroger',{'client_id':'someupdateduname','client_secret':'someupdatedpassword'})
+print(get_api_info('prod','kroger'))
+update_api_auth('dev','kroger',{'l':20})
 ""
